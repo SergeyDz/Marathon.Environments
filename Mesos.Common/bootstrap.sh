f99bcba63ca761e4c84ad7eecccdb375b4b845f5
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2015-12-07 15:00:31 +0200

    Multy-zookeeper configuration added

--- Mesos.Common/bootstrap.sh
+++ Mesos.Common/bootstrap.sh
@@ -4,12 +4,12 @@ yum -y install mesos device-mapper-event-libs docker
 cp /vagrant/host.conf /etc/hosts
 
 # point this node at the mesos master
-sed -i -e 's/localhost/192.168.33.10/g' /etc/mesos/zk
+sed -i -e 's/localhost:2181/10.1.0.178:2181,10.1.1.49:2181,10.1.0.77:2181/g' /etc/mesos/zk
 echo 'docker,mesos' | sudo tee /etc/mesos-slave/containerizers
 echo '5mins' | sudo tee /etc/mesos-slave/executor_registration_timeout
 
 
-sudo sed -i '1s/^/nameserver 192.168.33.10\n /' /etc/resolv.conf
+sudo sed -i '1s/^/nameserver 10.1.0.178\n /' /etc/resolv.conf
 yum -y install mc nano
 
 service mesos-slave start
--- Mesos.Common/marathon.deploy.sh
+++ Mesos.Common/marathon.deploy.sh
@@ -1,1 +1,1 @@
-curl -X POST http://192.168.33.10:8080/v2/apps -d @$1 -H "Content-type: application/json"
+curl -X POST http://10.1.0.178:8080/v2/apps -d @$1 -H "Content-type: application/json"
