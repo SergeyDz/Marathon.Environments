74493e12248035f43b4269068290057b47be3411
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2015-12-24 17:36:55 +0200

    Added marathon clean scripts with init zk

--- Mesos.Common/start.marathon.sh
+++ Mesos.Common/start.marathon.sh
@@ -1,5 +1,6 @@
-sudo service zookeeper 
-sudo service zookeeper-server
+sudo /bin/sh /usr/lib/zookeeper/bin/zkServer-initialize.sh
+sudo service zookeeper start
+sudo service zookeeper-server start
 sudo service mesos-master start
-sudo service mesos-slave start 
+sudo service mesos-slave start
 sudo service marathon start
